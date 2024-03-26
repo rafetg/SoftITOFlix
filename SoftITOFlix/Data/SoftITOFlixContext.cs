af29226bcec709655e5af06eca334b89c76adf73
rafetg <108269961+rafetg@users.noreply.github.com>
2024-03-26 11:49:41 +0300

    Add project files.

---
+++ SoftITOFlix/Data/SoftITOFlixContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SoftITOFlix.Models;
+
+namespace SoftITOFlix.Data
+{
+    public class SoftITOFlixContext : DbContext
+    {
+        public SoftITOFlixContext (DbContextOptions<SoftITOFlixContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SoftITOFlix.Models.Category> Category { get; set; } = default!;
+    }
+}
---
+++ SoftITOFlix/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftITOFlix.Models
+{
+    public class Category
+    {
+        public short Id { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        [StringLength(50,MinimumLength = 2)]
+        public string Name { get; set; } = "";
+    }
+}
